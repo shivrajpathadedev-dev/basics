5ba3c3353e70a2627816fe86b73fcd18c2d58b07
shivrajpathadedev-dev <shivrajpathade.dev@gmail.com>
2026-04-13 14:24:21 +0530

    cli no

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './shared/components/product/product.component';
+import { ProductsComponent } from './shared/components/products/products.component';
+import { userComponent } from './shared/components/users/user.component';
+import { VehicleComponent } from './shared/components/vehicle/vehicle.component';
+import { MountainComponent } from './shared/components/Mountain/Mountain.component';
+import { TestComponent } from './shared/components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductsComponent,
+    userComponent,
+    VehicleComponent,
+    MountainComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
